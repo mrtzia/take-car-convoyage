63fe0dbb5b6bab0589ea40f5b2960d5e1583485e
Eliott Martinez <eliottmartinez@macbookair.home>
2026-05-12 12:10:38 +0200

    Fix sticky: overflow-x hidden -> clip (compatible sticky), container 400vh

--- app/components/HowItWorks.tsx
+++ app/components/HowItWorks.tsx
@@ -105,7 +105,7 @@ export default function HowItWorks() {
     <div
       ref={containerRef}
       id="comment"
-      style={{ height: `${steps.length * 90}vh`, position: "relative" }}
+      style={{ height: `${steps.length * 100}vh`, position: "relative" }}
     >
 
       {/* Sticky viewport */}
